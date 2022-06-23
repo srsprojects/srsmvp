4abfae77984107b1c8ab91a9f768f047a631b497
zaghadon <zaghadon@gmail.com>
2022-06-23 21:55:17 +0100

    Upgraded Backpack Pro to Latest release

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 //use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Backpack\CRUD\app\Models\Traits\CrudTrait; // <------------------------------- this one
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,6 +14,8 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use CrudTrait;
+    use HasRoles;
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;

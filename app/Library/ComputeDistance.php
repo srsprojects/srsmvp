3160d7dc7d749e32a7165787d62eb304e43fb3db
zaghadon <zaghadon@gmail.com>
2023-05-02 03:19:27 +0100

    Added Library Codes for Helper functions

---
+++ app/Library/ComputeDistance.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Library;
+
+/* 
+Implementation of Vicenty Formula in php
+Great-circle distance. (2022, September 5). In Wikipedia. https://en.wikipedia.org/wiki/Great-circle_distance 
+
+Adapted by: David Eze Nzagha
+Note: Google Maps uses 6378137 as Earth radius. Passing this figure as the $earthRadius variable
+would enable consistency with Google Maps.
+*/
+
+class ComputeDistance
+{
+    /**
+     * Calculates the great-circle distance between two points, with
+     * the Vincenty formula.
+     * @param float $latitudeFrom Latitude of start point in [deg decimal]
+     * @param float $longitudeFrom Longitude of start point in [deg decimal]
+     * @param float $latitudeTo Latitude of target point in [deg decimal]
+     * @param float $longitudeTo Longitude of target point in [deg decimal]
+     * @param float $earthRadius Mean earth radius in [m]
+     * @return float Distance between points in [m] (same as earthRadius)
+     */
+    public static function vincentyGreatCircleDistance(
+        $latitudeFrom,
+        $longitudeFrom,
+        $latitudeTo,
+        $longitudeTo,
+        $earthRadius = 6378137
+    ) {
+        // convert from degrees to radians
+        $latFrom = deg2rad($latitudeFrom);
+        $lonFrom = deg2rad($longitudeFrom);
+        $latTo = deg2rad($latitudeTo);
+        $lonTo = deg2rad($longitudeTo);
+
+        $lonDelta = $lonTo - $lonFrom;
+        $a = pow(cos($latTo) * sin($lonDelta), 2) +
+            pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+        $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+
+        $angle = atan2(sqrt($a), $b);
+        return $angle * $earthRadius;
+    }
+}

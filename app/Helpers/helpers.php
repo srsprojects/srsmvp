d79d8df29a5c74117ae68a38fd997607f9322e14
zaghadon <zaghadon@gmail.com>
2023-05-16 09:00:03 +0100

    Cretaed Helper Functions

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Library\ReferralCode;
+use App\Models\Wallet;
+use Backpack\PermissionManager\app\Models\Role;
+
+function getRoles()
+{
+    return Role::all();
+}
+
+function getUserRoles(User $user)
+{
+    return $user->getRoles();
+}
+
+function generateRefCode($prefix): string
+{
+    $code = new ReferralCode($prefix);
+    return $code->referralCode;
+}
+
+function generateWalletNo($user_id)
+{
+    // attempt to create a referral code until the one you have is unique
+    do {
+        $wallet_no = strrev(substr((int)($user_id / 5 * 10) . rand(pow(10, 10), pow(10, 11) - 1), 0, 10));
+    } while (Wallet::where('wallet_no', $wallet_no)->exists() && strlen($wallet_no) == 10);
+
+    return $wallet_no;
+}
+
+function containsMaliciousInput(String $stringToCheck = null)
+{
+    static $badInputs = [
+        '<script>',
+        '</script>',
+        '<?>',
+        '<?php>',
+        '{{}}',        
+    ];
+    $occurrence = 0;
+    foreach($badInputs as $a){
+        $occurrence += substr_count($stringToCheck, $a);
+    }
+    return $occurrence > 0;
+}

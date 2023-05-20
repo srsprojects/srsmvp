514e3dc5ebe264cf83d3a1e4c8adb4e25628f8ac
zaghadon <zaghadon@gmail.com>
2023-05-20 23:18:46 +0100

    Created User QRCode Library

---
+++ app/Library/UserQRCode.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Library;
+
+use BaconQrCode\Renderer\Color\Rgb;
+use BaconQrCode\Renderer\Image\SvgImageBackEnd;
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\RendererStyle\Fill;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
+
+trait UserQRCode
+{
+    /**
+     * Get the QR code SVG of the user's Phone Number.
+     *
+     * @return string
+     */
+    public function userQrCodeSvg()
+    {
+        $svg = (new Writer(
+            new ImageRenderer(
+                new RendererStyle(192, 0, null, null, Fill::uniformColor(new Rgb(255, 255, 255), new Rgb(45, 55, 72))),
+                new SvgImageBackEnd
+            )
+        ))->writeString($this->phone);
+
+        return trim(substr($svg, strpos($svg, "\n") + 1));
+    }
+}

5b1da623fd3d7da2b41336c0a6d61c2b16fd9196
zaghadon <zaghadon@gmail.com>
2023-06-09 14:10:21 +0100

    Started a Withdrawal Modal

---
+++ resources/views/components/modals/withdraw-modal.blade.php
@@ -0,0 +1,14 @@
+<!-- Withdrawal Account modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="withdraw">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <a href="#" class="close" data-bs-dismiss="modal"><em class="icon ni ni-cross-sm"></em></a>
+            <div class="modal-body modal-body-md">
+                <h5 class="title mb-4">Enter Account Details</h5>
+                <div class="nk-country-region">
+                    
+                </div>
+            </div>
+        </div><!-- .modal-content -->
+    </div><!-- .modla-dialog -->
+</div><!-- .modal -->

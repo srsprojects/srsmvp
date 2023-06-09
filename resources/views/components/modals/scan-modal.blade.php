344109db49f178ddb34dc5003da8d3e5a3a93096
zaghadon <zaghadon@gmail.com>
2023-06-09 15:24:38 +0100

    Created a Scanner Modal for QRCode Scan

---
+++ resources/views/components/modals/scan-modal.blade.php
@@ -0,0 +1,14 @@
+<!-- Withdrawal Account modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="scan">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <a href="#" class="close" data-bs-dismiss="modal"><em class="icon ni ni-cross-sm"></em></a>
+            <div class="modal-body modal-body-md">
+                <h5 class="title mb-4">Scan SRS User QR</h5>
+                <div class="nk-country-region center">
+                    <div id="qr-reader" style="width: 60%"></div>
+                </div>
+            </div>
+        </div><!-- .modal-content -->
+    </div><!-- .modla-dialog -->
+</div><!-- .modal -->
